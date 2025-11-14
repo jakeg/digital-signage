6558e63e8a5615a86b37d7037f566df02c4c406e
Jake Gordon <github@jakeg.co.uk>
2025-11-14 00:42:01 +0000

    quiet and on same screen

--- slideshow.sh
+++ slideshow.sh
@@ -2,6 +2,6 @@
 
 while true; do
   # Find all images and display with fim
-  fim -a -R 5 -T 2 ~/digital-signage/slides/*.png 2>/dev/null
+  fim -a -R 5 -q ~/digital-signage/slides/*.png 2>/dev/null
   sleep 5
 done
